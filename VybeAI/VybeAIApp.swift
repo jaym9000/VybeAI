1863c7bf74eb2812af0f19233eab9db798a4c3cc
jm <jean_marcc@hotmail.com>
2025-03-26 21:53:53 -0300

    Initial Commit

---
+++ VybeAI/VybeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VybeAIApp.swift
+//  VybeAI
+//
+//  Created by JM Mahoro on 2025-03-26.
+//
+
+import SwiftUI
+
+@main
+struct VybeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VybeAITests/VybeAITests.swift
@@ -0,0 +1,17 @@
+//
+//  VybeAITests.swift
+//  VybeAITests
+//
+//  Created by JM Mahoro on 2025-03-26.
+//
+
+import Testing
+@testable import VybeAI
+
+struct VybeAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
